cd11f49904e0f0a260e7af01474ba2882bc52ec0
Dhimas Ferdiansyah <starblackdest@gmail.com>
2023-10-08 12:20:41 +0700

    update

---
+++ 05-Operators.js
@@ -0,0 +1,41 @@
+// Contoh 1
+{
+  let x = 10;
+  let y = 20;
+  let z = x + y;
+
+  console.log(z); // 30
+}
+
+// Contoh 2
+{
+  let x = 10;
+  let y = 20;
+
+  console.log(x + y); // 30
+}
+
+// Contoh 3
+{
+  let a = 10;
+  let b = (5 + 10) * a;
+
+  console.log(b); // 150
+}
+
+// Contoh 4
+{
+  let text1 = "Dhimas";
+  let text2 = "Ferdiansyah";
+  let text3 = text1 + " " + text2;
+
+  console.log(text3); // Dhimas Ferdiansyah
+}
+
+// Contoh 5
+{
+  let a = "Hari minggu ";
+  a += "sangat menyenangkan";
+
+  console.log(a); // Hari minggu sangat menyenangkan
+}
